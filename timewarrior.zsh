f45d6262aaf6cf6085fe2df527dbb24e6ea379bd
Sven Hergenhahn <sven.hergenhahn@dm.de>
2018-04-20 14:45:11 +0200

    use tws_* for sum, twsh for shorten as it will be used much less often

--- timewarrior.zsh
+++ timewarrior.zsh
@@ -5,12 +5,12 @@
 
 alias tw=timew
 alias twsm='timew summary'
-alias twsmi='timew summary :ids'
-alias twsmy='timew summary :yesterday'
-alias twsmw='timew summary :week'
+alias twsi='timew summary :ids'
+alias twsy='timew summary :yesterday'
+alias twsw='timew summary :week'
 alias twa='timew start'
 alias two='timew stop'
 alias twc='timew continue'
 alias twt='timew track'
 alias twl='timew lengthen'
-alias tws='timew shorten'
+alias twsh='timew shorten'
