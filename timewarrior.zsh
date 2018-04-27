fe4affd1f0453b8543baf56fef6dd8a6edc3c502
Sven Hergenhahn <sven.hergenhahn@dm.de>
2018-04-27 15:13:02 +0200

    fix summary alias

--- timewarrior.zsh
+++ timewarrior.zsh
@@ -4,10 +4,12 @@
 #
 
 alias tw=timew
-alias twsm='timew summary'
+
+alias tws='timew summary'
 alias twsi='timew summary :ids'
 alias twsy='timew summary :yesterday'
 alias twsw='timew summary :week'
+
 alias twa='timew start'
 alias two='timew stop'
 alias twc='timew continue'
